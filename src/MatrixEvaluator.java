fbdda30e24022b0401107de770b32892e78fe41a
Garrett W. Sangala <garrettwsangala@gmail.com>
2024-03-25 23:56:58 -0400

    Initial commit

---
+++ src/MatrixEvaluator.java
@@ -0,0 +1,27 @@
+/**
+ * MatrixEvaluator
+ * @author Garrett W. Sangala
+ * @version 0.1
+ * @since 2024-03-25
+ *
+ * This Java program prompts a user for a file containing a boolean matrix, and then for a sequence of vertices.
+ * The program then determines if and which of the following the given sequence of vertices represents:
+ * -Open Walk
+ * -Closed walk
+ * -Trail
+ * -Circuit
+ * -Path
+ * -Cycle
+ *
+ * The program will also generate the reflexive and symmetric closures of the input matrix and output the
+ * matrix closures into a file for each.
+ */
+public class MatrixEvaluator {
+
+    public static void main(String args[]) {
+
+
+
+
+    }
+}
