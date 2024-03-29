8441bab68a82b7a8c2d943c34d6a5389dcadce50
Garrett W. Sangala <garrettwsangala@gmail.com>
2024-03-29 00:26:26 -0400

    Basic File IO added

--- src/MatrixEvaluator.java
+++ src/MatrixEvaluator.java
@@ -16,9 +16,43 @@
  * The program will also generate the reflexive and symmetric closures of the input matrix and output the
  * matrix closures into a file for each.
  */
+
+import java.io.*;
+import java.util.Scanner;
+
 public class MatrixEvaluator {
 
     public static void main(String args[]) {
+
+        // Read the matrix from the file into a 2D array
+        int matrixRows = 10;
+        int matrixColumns = 10;
+        int[][] matrix = new int[matrixRows][matrixColumns];
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter the name of the file that has the matrix: ");
+        String fileName = userInput.nextLine();
+        File matrixFile = new File(fileName);
+
+        BufferedReader matrixInput = null;
+        try {
+            matrixInput = new BufferedReader(new FileReader(matrixFile));
+
+            String fileLine;
+            while ((fileLine = matrixInput.readLine()) != null) {
+                System.out.println(fileLine);
+                //Convert the line printing to reading each char into a 2d array
+                //Then print array to test
+            }
+
+        } catch (IOException error) {
+            error.printStackTrace();
+        } finally {
+            try {
+                matrixInput.close();
+            } catch (IOException error) {
+                error.printStackTrace();
+            }
+        }
 
 
 
